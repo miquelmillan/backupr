587ee8c104b1281019ac4ca797538a6f721fdae7
mmillan <mmillan@cobugs.io>
2019-06-02 20:14:31 +0200

    Basic CLI setup

--- backupr-cli/src/main/java/com/miquelmillan/App.java
+++ backupr-cli/src/main/java/com/miquelmillan/App.java
@@ -1,12 +1,31 @@
 package com.miquelmillan;
 
-/**
- * Hello world!
- *
- */
 
-public class App {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App
+        implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory
+            .getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LOG.info("STARTING THE APPLICATION");
+        SpringApplication.run(App.class, args);
+        LOG.info("APPLICATION FINISHED");
+    }
+
+    @Override
+    public void run(String... args) {
+        LOG.info("EXECUTING : command line runner");
+
+        for (int i = 0; i < args.length; ++i) {
+            LOG.info("args[{}]: {}", i, args[i]);
+        }
     }
 }
